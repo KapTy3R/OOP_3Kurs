9359f2198307e357dd0026d75779a6d64a0cbd9a
KapTy3R <marselle.mc4ep@gmail.com>
2025-01-05 14:03:30 +0300

    Промежуточные правки

--- Data.cs
+++ Data.cs
@@ -8,6 +8,8 @@ namespace Task1
 {
     internal abstract class Data
     {
+
+
         public abstract void Read(ref List<Buyers> buyers, ref List<Items> items, ref List<Orders> orders);
         public abstract void Write(List<Buyers> buyers, List<Items> items, List<Orders> orders);
     }
